0ffba613a160f505266d34ae5343bd1dd913f8ee
jits <jits@example.com>
2025-06-05 20:38:12 +0400

    updated tasks

--- playbooks/files/sql/insert_rows.sql
+++ playbooks/files/sql/insert_rows.sql
@@ -1,5 +1,4 @@
 -- insert_rows.sql
-INSERT INTO &tabname (name, department, salary) VALUES ('&emp1', '&dept1', &sal1);
-INSERT INTO &tabname (name, department, salary) VALUES ('&emp2', '&dept2', &sal2);
+INSERT INTO &tabname (name, department, salary) VALUES ('&emp1', &sal1);
 
 COMMIT;
